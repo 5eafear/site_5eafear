7282ff94f9c3d6f927e259c0d79cdb86d4f09b8c
5eafear <81483532+5eafear@users.noreply.github.com>
2022-05-23 21:23:05 +0300

    Delete fillups directory

--- fillups/vecof.net (1)/inc/bd.php
+++
@@ -1,9 +0,0 @@
-<?php
-mysql_connect("5eafear.xyz", "airloto", "MySQL1MySQL")//параметры в скобках ("хост", "имя пользователя", "пароль")
-or die("<p>Ошибка подключения к базе данных! " . mysql_error() . "</p>");
-
-
-mysql_select_db("fghfghhg")//параметр в скобках ("имя базы, с которой соединяемся")
- or die("<p>Ошибка выбора базы данных! ". mysql_error() . "</p>");
-mysql_query("SET NAMES utf8");
-?>
